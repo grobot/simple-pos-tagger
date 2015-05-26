70f8c9f0a1ded561cbb6484a711b74fe3a492051
Hugo ter Doest <hwl.ter.doest@gmail.com>
2015-05-26 23:30:55 +0200

    Made the tagger synchronous

--- spec/SimplePOSTagger_spec.js
+++ spec/SimplePOSTagger_spec.js
@@ -1,5 +1,5 @@
 /*
-    Unit test for FunctionWordTagger
+    Unit test for SimplePOSTagger
     Copyright (C) 2015 Hugo W.L. ter Doest
 
     This program is free software: you can redistribute it and/or modify
@@ -19,7 +19,7 @@
 var FWT = require('../lib/SimplePOSTagger');
 
 // Path to the lexicon files
-var path_to_lexicon = "/home/hugo/Workspace/simple_pos_tagger/data/";
+var path_to_lexicon = "/home/hugo/Workspace/simple-pos-tagger/data/";
 //var path_to_lexicon = "/Eclipse Workspace/function-word-tagger/data/";
 
 var config_files = [path_to_lexicon + "Dutch/lexicon_files.json", 
@@ -33,86 +33,82 @@ describe("The function word tagger", function() {
 
   it("Reads a Dutch lexicon and tags sentences", function() {
     // Create tagger
-    new FWT(config_files[0], function(tagger) {
-      var tagged_sentence = tagger.tag_sentence(sentences[0]);
-      expect(tagged_sentence).toEqual([ [ 'ik', [ 'persoonlijk_voornaamwoord' ] ],
-                                        [ 'zie', undefined ],
-                                        [ 'de', [ 'lidwoord' ] ],
-                                        [ 'man', undefined ],
-                                        [ 'met', [ 'voorzetsel' ] ],
-                                        [ 'de', [ 'lidwoord' ] ],
-                                        [ 'verrekijker', undefined ] ]);
-    });
+    var tagger = new FWT(config_files[0]);
+    var tagged_sentence = tagger.tag_sentence(sentences[0]);
+    expect(tagged_sentence).toEqual([ [ 'ik', [ 'persoonlijk_voornaamwoord' ] ],
+                                      [ 'zie', undefined ],
+                                      [ 'de', [ 'lidwoord' ] ],
+                                      [ 'man', undefined ],
+                                      [ 'met', [ 'voorzetsel' ] ],
+                                      [ 'de', [ 'lidwoord' ] ],
+                                      [ 'verrekijker', undefined ] ]);
   });
   
   it("Reads an English lexicon and tags sentences", function() {
     // Create tagger
-    new FWT(config_files[0], function(tagger) {
-      var tagged_sentence = tagger.tag_sentence(sentences[1]);
-      expect(tagged_sentence).toEqual([ [ 'I', [ 'personal_pronoun' ] ],
-                                        [ 'see', undefined ],
-                                        [ 'the', [ 'determiner' ] ],
-                                        [ 'man', undefined ],
-                                        [ 'with', [ 'preposition' ] ],
-                                        [ 'the', [ 'determiner' ] ],
-                                        [ 'telescope', undefined ] ]);
-      var chart = tagger.tag_sentence(sentences[2]);
-      expect(chart.outgoing_edges).toEqual(
-      [
-        {
-          "CYK(preposition, 0, 1)": [
-            {
-              "id": "CYK(preposition, 0, 1)",
-              "name": "preposition",
-              "children": [],
-              "data": {
-                "from": 0,
-                "to": 1,
-                "rule": {
-                  "lhs": "preposition",
-                  "rhs": "in"
-                }
+    var tagger = new FWT(config_files[1]);
+    var tagged_sentence = tagger.tag_sentence(sentences[1]);
+    expect(tagged_sentence).toEqual([ [ 'I', [ 'personal_pronoun' ] ],
+                                      [ 'see', undefined ],
+                                      [ 'the', [ 'determiner' ] ],
+                                      [ 'man', undefined ],
+                                      [ 'with', [ 'preposition' ] ],
+                                      [ 'the', [ 'determiner' ] ],
+                                      [ 'telescope', undefined ] ]);
+    var chart = tagger.tag_sentence_chart(sentences[2]);
+    expect(chart.outgoing_edges).toEqual(
+    [
+      {
+        "CYK(preposition, 0, 1)": [
+          {
+            "id": "CYK(preposition, 0, 1)",
+            "name": "preposition",
+            "children": [],
+            "data": {
+              "from": 0,
+              "to": 1,
+              "rule": {
+                "lhs": "preposition",
+                "rhs": "in"
               }
             }
-          ],
-          "CYK(preposition, 0, 3)": [
-            {
-              "id": "CYK(preposition, 0, 3)",
-              "name": "preposition",
-              "children": [],
-              "data": {
-                "from": 0,
-                "to": 3,
-                "rule": {
-                  "lhs": "preposition",
-                  "rhs": "in accordance with"
-                }
+          }
+        ],
+        "CYK(preposition, 0, 3)": [
+          {
+            "id": "CYK(preposition, 0, 3)",
+            "name": "preposition",
+            "children": [],
+            "data": {
+              "from": 0,
+              "to": 3,
+              "rule": {
+                "lhs": "preposition",
+                "rhs": "in accordance with"
               }
             }
-          ]
-        },
-        {},
-        {
-          "CYK(preposition, 2, 3)": [
-            {
-              "id": "CYK(preposition, 2, 3)",
-              "name": "preposition",
-              "children": [],
-              "data": {
-                "from": 2,
-                "to": 3,
-                "rule": {
-                  "lhs": "preposition",
-                  "rhs": "with"
-                }
+          }
+        ]
+      },
+      {},
+      {
+        "CYK(preposition, 2, 3)": [
+          {
+            "id": "CYK(preposition, 2, 3)",
+            "name": "preposition",
+            "children": [],
+            "data": {
+              "from": 2,
+              "to": 3,
+              "rule": {
+                "lhs": "preposition",
+                "rhs": "with"
               }
             }
-          ]
-        },
-        {}
-      ]);
-    });
+          }
+        ]
+      },
+      {}
+    ]);
   });
 });
-
-
